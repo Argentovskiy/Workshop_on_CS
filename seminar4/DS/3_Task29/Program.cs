e9a991fff87708abd0505eccc03c89e8472a1958
Argent_NB <agricc@mail.ru>
2023-07-05 17:19:31 +0300

    работаю

--- seminar4/DS/3_Task29/Program.cs
+++ seminar4/DS/3_Task29/Program.cs
@@ -6,15 +6,42 @@
 // ??? Понял задачу так что заданные числа в примере нужно вести прямо в програме. 
 // Для удобства написал код ввода с консоли.
 
-Console.WriteLine("Введите через запятую вашь массив");
-
-string a=Console.ReadLine();
-string b = ",";
-for(int i=0; i<a.Length; i++){
-    if(a[i] == b) {        
-    Console.WriteLine($"Длина массива = {a.Length} ");
-    Console.WriteLine(a.Remove(i,i));
-
+//=== Удаляем запятые ====
+int[] dell (string a){
+char b = ',';
+int[] array= new int[100];
+int j=0;
+int tt=0,td;
+//string temp=string.Empty;
+for(int i=0; i<a.Length-1; i++){
+      if(a[i] == b) {  
+        tt=0;
+        for(int n=0; n<i; n++){ td=tt*10; tt=td+a[n];
+        System.Console.WriteLine($"td= {td}");
+        System.Console.WriteLine($"a= {a[n]}"); }
+        System.Console.WriteLine($"tt= {tt}");
+                array[j]=tt/10;
+        //System.Console.WriteLine($"tt= {tt}");
+        a=a.Remove(0,i+1);
+        System.Console.WriteLine(a);
+        System.Console.WriteLine($"элемент {j} = {array[j]} ");
+        j++;
+        i=0;
     }
 }
+return array;
+}
+
+void PrintArrya(int[] array){
+    for(int i=0; i<array.Length;i++){
+        System.Console.Write($" {array[i]}");
+    }
+}
+
+Console.WriteLine("Введите через запятую вашь массив");
+
+string strA=Console.ReadLine();
+//int[] arrayB =dell(strA);
+
+PrintArrya(dell(strA));
 //int[] array=new int[4];
