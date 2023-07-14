6e312da343ec1a360ef0a131f70f95de440d3d77
Argent_NB <agricc@mail.ru>
2023-07-14 13:51:20 +0300

    омашние задание к меминару 6 выполнено

---
+++ seminar6/DS/1_Task41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+//========================================================
+System.Console.WriteLine("Сколько числе вы хотите ввести? ");
+int M=int.Parse(Console.ReadLine());
+int[] Array=new int[M];
+int count=0;
+System.Console.WriteLine($"Введите {M} чисел");
+for (int i = 0; i < M; i++)
+{
+    Array[i]=int.Parse(Console.ReadLine());
+    if(Array[i]>0) count++;
+}
+System.Console.WriteLine("чисел больше 0 -> "+count+"  ");
+
+// готово
---
+++ seminar6/DS/2_Task43/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+System.Console.WriteLine("Введите переменные :");
+System.Console.Write("b1=");
+int b1=int.Parse(Console.ReadLine());
+System.Console.Write("k1=");
+int k1=int.Parse(Console.ReadLine());
+System.Console.Write("b2=");
+int b2=int.Parse(Console.ReadLine());
+System.Console.Write("k2=");
+double k2=int.Parse(Console.ReadLine());
+
+if((k1 == k2) && (b1 == b2)) System.Console.WriteLine("Прямые совпадают");
+else if (k1==k2)System.Console.WriteLine("Прямые параллельны");
+else
+    {
+        double x=(b2-b1)/(k1-k2);
+        double y=(k1*(b2-b1))/(k1-k2)+b1;
+        System.Console.WriteLine("(x="+x+"; y="+y+")");
+
+}
+
+// Готово
