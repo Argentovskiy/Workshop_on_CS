d53f20436768081e46c124a42891520f291536eb
Argent <agricc@mail.ru>
2023-07-30 19:40:12 +0300

    омашние задание к семинару 9 выполнено

---
+++ seminar9/1_Task67/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+//Сумма чисел
+int SumNumber(int n)
+{
+    if((n/10)==0) return n;
+    int sum=0;
+    sum=SumNumber(n/10)+n%10;
+    return sum;
+}
+
+
+//======
+System.Console.WriteLine("Введите число сумму чисел которого нужно посчитать: ");
+
+int number =int.Parse(Console.ReadLine());
+
+System.Console.WriteLine(SumNumber(number));
